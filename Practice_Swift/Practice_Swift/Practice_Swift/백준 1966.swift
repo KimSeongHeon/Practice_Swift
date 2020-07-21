1f2119a1f295189a7cbea9603461673d0e123a0c
KimSeongHeon <47879772+KimSeongHeon@users.noreply.github.com>
2020-07-21 21:25:16 +0900

    2020/07/20
    2문제 풀이

--- Practice_Swift/Practice_Swift/Practice_Swift/백준 1966.swift
+++ Practice_Swift/Practice_Swift/Practice_Swift/백준 1966.swift
@@ -1,5 +1,5 @@
 //
-//  백준 1966.swift
+//  main.swift
 //  Practice_Swift
 //
 //  Created by 김성헌 on 2020/07/20.
@@ -7,13 +7,64 @@
 //
 
 import Foundation
-
+public struct Queue<T>{
+    internal var data = Array<T>()
+    public init(){}
+    public mutating func dequeue() -> T?{
+        return data.removeFirst()
+    }
+    public func peek() -> T?{
+        return data.first
+    }
+    public mutating func enqueue(element:T){
+        data.append(element)
+    }
+    public mutating func clear(){
+        data.removeAll()
+    }
+    public func count() -> Int{
+        return data.count
+    }
+    public func isempty() -> Bool{
+        return data.count == 0 ? true : false
+    }
+    public func getIndex(Index:Int)->T?{
+        return data[data.index(data.startIndex, offsetBy: Index)]
+    }
+}
+var arr  = [Int]();
 var tc = readLine()!
-for i in 0...Int(tc)-1{
-    var nm = readLine()!
+for _ in 0...Int(tc)!-1{
+    let nm = readLine()!
     let n = nm.components(separatedBy: " ")[0]
     let m = nm.components(separatedBy: " ")[1]
-    for j in 0..Int(m)-1{
-        
+    let element = readLine()!.components(separatedBy: " ")
+    var queue = Queue<(Int,Int)>()
+    var index = 0;
+    for item in element{
+        queue.enqueue(element: (index,Int(item)!))
+        index += 1
     }
+    var ans = 0;
+    while(!queue.isempty()){
+        var check = false;
+        for k in 0...queue.count()-1{
+            if queue.peek()!.1 < queue.getIndex(Index: k)!.1{
+                queue.enqueue(element: queue.dequeue()!)
+                check = true;
+                break;
+            }
+        }
+        if !check{
+            if queue.peek()!.0 == Int(m)!{
+                arr.append(ans+1)
+                break;
+            }
+            queue.dequeue()
+            ans += 1;
+        }
+    }
+}
+for item in arr{
+    print("\(item)")
 }
